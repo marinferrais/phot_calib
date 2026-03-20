65f4af7633f02419b177b5bbd4cea2051c0df7fc
marinferrais <ferrais-marin@hotmail.com>
2026-03-20 19:21:01 -0400

    fixed issues with fits file not properly closed

--- src/phot_calib/fits_platesolve.py
+++ src/phot_calib/fits_platesolve.py
@@ -63,104 +63,112 @@ def fits_platesolve(
     -------
 
     """
-
     file = filenames[len(filenames) // 2]
-    data = fits.getdata(file)
-    header = fits.getheader(file)
-    # get telescope specific params dict
-    obsparam = telescope_parameters[f"{header["TELESCOP"]} {header["INSTRUME"]}"]
-    # get pixel scale in degree
-    pixel_scale = header[obsparam["binning"][0]] * obsparam["px_scale"] / 3600
-    # size of the field-of-view
-    fov = data.shape[1] * pixel_scale
-    # RA/Dec coordinates of the image
-    ra = header[obsparam["ra"]]
-    dec = header[obsparam["dec"]]
-    if obsparam["radec_separator"] == "XXX":
-        center = SkyCoord(ra, dec, unit=("deg", "deg"))
-    else:
-        center = SkyCoord(ra, dec, unit=("h", "deg"))
-    if verbose:
-        print(f"Image center : {center.to_string('hmsdms')}")
-    all_radecs = gaia_radecs(center, 1.5 * fov)
-    # we only keep stars 0.01 degree apart from each other
-    all_radecs = sparsify(all_radecs, 0.01)
+    with fits.open(file) as hdul:
+        data = hdul[0].data
+        header = hdul[0].header
+        # get telescope specific params dict
+        obsparam = telescope_parameters[f"{header["TELESCOP"]} {header["INSTRUME"]}"]
+        # get pixel scale in degree
+        pixel_scale = header[obsparam["binning"][0]] * obsparam["px_scale"] / 3600
+        # size of the field-of-view
+        fov = data.shape[1] * pixel_scale
+        # RA/Dec coordinates of the image
+        ra = header[obsparam["ra"]]
+        dec = header[obsparam["dec"]]
+        if obsparam["radec_separator"] == "XXX":
+            center = SkyCoord(ra, dec, unit=("deg", "deg"))
+        else:
+            center = SkyCoord(ra, dec, unit=("h", "deg"))
+        if verbose:
+            print(f"Image center : {center.to_string('hmsdms')}")
+        all_radecs = gaia_radecs(center, 1.5 * fov)
+        # we only keep stars 0.01 degree apart from each other
+        all_radecs = sparsify(all_radecs, 0.01)
 
     for file in tqdm(filenames):
         if verbose:
-            print(f"Plate-solving {file} ...")
-        # read image data and header
-        data = fits.getdata(file)
-        header = fits.getheader(file)
+            tqdm.write(f"Plate-solving {file} ...")
+        with fits.open(file, mode="update") as hdul:
+            # read image data and header
+            data = hdul[0].data
+            header = hdul[0].header
 
-        if safe_mode:
-            # get telescope specific params dict
-            obsparam = telescope_parameters[
-                f"{header["TELESCOP"]} {header["INSTRUME"]}"
-            ]
-            # get pixel scale in degree
-            pixel_scale = header[obsparam["binning"][0]] * obsparam["px_scale"] / 3600
-            # size of the field-of-view
-            fov = data.shape[1] * pixel_scale
-            # RA/Dec coordinates of the image
-            ra = header[obsparam["ra"]]
-            dec = header[obsparam["dec"]]
-            if obsparam["radec_separator"] == "XXX":
-                center = SkyCoord(ra, dec, unit=("deg", "deg"))
-            else:
-                center = SkyCoord(ra, dec, unit=("h", "deg"))
+            if safe_mode:
+                # get telescope specific params dict
+                obsparam = telescope_parameters[
+                    f"{header["TELESCOP"]} {header["INSTRUME"]}"
+                ]
+                # get pixel scale in degree
+                pixel_scale = (
+                    header[obsparam["binning"][0]] * obsparam["px_scale"] / 3600
+                )
+                # size of the field-of-view
+                fov = data.shape[1] * pixel_scale
+                # RA/Dec coordinates of the image
+                ra = header[obsparam["ra"]]
+                dec = header[obsparam["dec"]]
+                if obsparam["radec_separator"] == "XXX":
+                    center = SkyCoord(ra, dec, unit=("deg", "deg"))
+                else:
+                    center = SkyCoord(ra, dec, unit=("h", "deg"))
+                if verbose:
+                    tqdm.write(f"Image center : {center.to_string('hmsdms')}")
+                all_radecs = gaia_radecs(center, 1.5 * fov)
+                # we only keep stars 0.01 degree apart from each other
+                all_radecs = sparsify(all_radecs, 0.01)
+
+            # detect stars coordinates
+            regions = detection.stars_detection(data, threshold=20, opening=2)
+            regions = sorted(
+                regions, key=lambda x: x.image_intensity.sum(), reverse=True
+            )
+            coords = np.array([r.centroid_weighted[::-1] for r in regions])[0:nstars]
+
+            # compute WCS
+            try:
+                with suppress_output():
+                    wcs = compute_wcs(coords, all_radecs[0:nstars], tolerance=10)
+            except Exception as e:
+                if verbose:
+                    tqdm.write(f"Error occurred while plate-solving {file}: {e}")
+                continue
+            if wcs is None:
+                if verbose:
+                    tqdm.write(f"Plate-solving failed for {file}")
+                continue
+
+            # save as FITS
+            hdul[0].header.update(wcs.to_header())
+            hdul.flush()  # write changes to disk
+
+            if display:
+                # TODO : display the plate-solved image
+                radecs_xy = np.array(wcs.world_to_pixel_values(all_radecs))[0:300]
+                fig, ax = plt.subplots(figsize=(12, 12), subplot_kw={"projection": wcs})
+                ax.imshow(viz.z_scale(data), cmap="Greys_r", origin="lower")
+                # Plot catalog stars
+                viz.plot_marks(*radecs_xy.T, color="y")
+                # Add coordinate grid
+                ax.coords.grid(True, color="white", alpha=0.2, linestyle="--")
+                fig.suptitle(file)
+                ax.set_xlabel("RA")
+                ax.set_ylabel("Dec")
+                # Format tick labels
+                ra = ax.coords["ra"]
+                dec = ax.coords["dec"]
+                ra.set_format_unit("hourangle")
+                dec.set_format_unit("deg")
+                ra.set_major_formatter("hh:mm:ss")
+                dec.set_major_formatter("dd:mm:ss")
+                # Tick spacing
+                ra.set_ticks(spacing=1 * u.arcmin)
+                dec.set_ticks(spacing=1 * u.arcmin)
+                fig.tight_layout()
+                plt.show()
+
             if verbose:
-                print(f"Image center : {center.to_string('hmsdms')}")
-            all_radecs = gaia_radecs(center, 1.5 * fov)
-            # we only keep stars 0.01 degree apart from each other
-            all_radecs = sparsify(all_radecs, 0.01)
-
-        # detect stars coordinates
-        regions = detection.stars_detection(data, threshold=20, opening=2)
-        regions = sorted(regions, key=lambda x: x.image_intensity.sum(), reverse=True)
-        coords = np.array([r.centroid_weighted[::-1] for r in regions])[0:nstars]
-
-        # compute WCS
-        try:
-            with suppress_output():
-                wcs = compute_wcs(coords, all_radecs[0:nstars], tolerance=10)
-        except Exception as e:
-            if verbose:
-                print(f"Error occurred while plate-solving {file}: {e}")
-            continue
-
-        # save as FITS
-        header.update(wcs.to_header())
-        hdu = fits.PrimaryHDU(data=data, header=header)
-        hdu.writeto(file, overwrite=True)
-
-        if display:
-            # TODO : display the plate-solved image
-            radecs_xy = np.array(wcs.world_to_pixel_values(all_radecs))[0:300]
-            fig, ax = plt.subplots(figsize=(12, 12), subplot_kw={"projection": wcs})
-            ax.imshow(viz.z_scale(data), cmap="Greys_r", origin="lower")
-            # Plot catalog stars
-            viz.plot_marks(*radecs_xy.T, color="y")
-            # Add coordinate grid
-            ax.coords.grid(True, color="white", alpha=0.2, linestyle="--")
-            fig.suptitle(file)
-            ax.set_xlabel("RA")
-            ax.set_ylabel("Dec")
-            # Format tick labels
-            ra = ax.coords["ra"]
-            dec = ax.coords["dec"]
-            ra.set_format_unit("hourangle")
-            dec.set_format_unit("deg")
-            ra.set_major_formatter("hh:mm:ss")
-            dec.set_major_formatter("dd:mm:ss")
-            # Tick spacing
-            ra.set_ticks(spacing=1 * u.arcmin)
-            dec.set_ticks(spacing=1 * u.arcmin)
-            fig.tight_layout()
-            plt.show()
-
-        if verbose:
-            print("Plate-solving done")
+                tqdm.write("Plate-solving done")
 
 
 #
