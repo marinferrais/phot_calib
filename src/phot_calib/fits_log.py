097221b1a23f334152f8c38fc945aac95d96714b
marinferrais <ferrais-marin@hotmail.com>
2026-03-20 17:52:30 -0400

    update shebang to use uv env

--- src/phot_calib/fits_log.py
+++ src/phot_calib/fits_log.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
 
 """LOG_IMAGES - Create a log of FITS images.
 
--- src/phot_calib/fits_trim.py
+++ src/phot_calib/fits_trim.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
 
 """FITS_TRIM - Trim fits images
 
